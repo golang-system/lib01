554d9ae508d9d37f03207030a4240ccf99088314
JavaCommons Technologies <javacommons@gmail.com>
2024-01-25 10:24:42 +0900

    Release v0.1.0

--- release.sh
+++ release.sh
@@ -2,8 +2,9 @@
 set -uvx
 set -e
 cwd=`pwd`
-ts=`date "+%Y.%m%d.%H%M.%S"`
-version="${ts}"
+#ts=`date "+%Y.%m%d.%H%M.%S"`
+#version="${ts}"
+version=0.1.0
 git add .
 git commit -m"Release v$version"
 git tag -a v$ts -mv$version
