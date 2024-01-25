721a076abd2fc3fc6234e66bf7b59db9dd5bb77d
JavaCommons Technologies <javacommons@gmail.com>
2024-01-25 10:25:06 +0900

    Release v0.1.0

--- release.sh
+++ release.sh
@@ -7,7 +7,7 @@ cwd=`pwd`
 version=0.1.0
 git add .
 git commit -m"Release v$version"
-git tag -a v$ts -mv$version
+git tag -a v$version -mv$version
 git push origin v$version
 git push
 git remote -v
